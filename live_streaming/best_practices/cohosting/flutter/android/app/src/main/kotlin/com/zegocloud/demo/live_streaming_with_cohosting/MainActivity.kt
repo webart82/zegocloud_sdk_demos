a5f1ec216f3a51cfbf5364a2bbdda6beb73ca442
careyxi <careyxi@zego.im>
2023-03-31 11:26:37 +0800

    create flutter live streaming cohosting.

---
+++ live_streaming/best_practices/cohosting/flutter/android/app/src/main/kotlin/com/zegocloud/demo/live_streaming_with_cohosting/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zegocloud.demo.live_streaming_with_cohosting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ live_streaming/best_practices/cohosting/flutter/lib/zego_sdk_key_center.dart
@@ -0,0 +1,5 @@
+class SDKKeyCenter {
+  static const int appID = ;
+  static const String secret = '';
+  static const String appSign = '';
+}
